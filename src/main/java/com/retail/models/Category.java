72322ae3f21f94f9775f4538471c2736a491ddd0
Divij <divijsehgal2599@gmail.com>
2022-06-13 11:17:03 +0530

    final commit

---
+++ src/main/java/com/retail/models/Category.java
@@ -0,0 +1,29 @@
+package com.retail.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String categoryName;
+
+    private String categoryDescription;
+
+    @OneToMany(mappedBy = "products")
+    private List<Product> products;
+
+}
